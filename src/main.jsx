2cdc0f642ecc1a49b823297b9bdbe70903228682
Nizam Uddin Imran <39611124+mr-Imran@users.noreply.github.com>
2024-10-27 09:26:32 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,25 @@ import './index.css'
 import App from './App.jsx'
 import { Analytics } from "@vercel/analytics/react"
 
+const playerdata = fetch('https://s6.imrann.my.id/player.json')
+.then((response) => response.json())
+.then((data) => data.players)
+.catch((error) => {
+  console.error('Error:', error);
+});
+console.log(playerdata);
+
+
+// DO YOUR SELF DO NOT COPY PASTE, 
+// I'M NOT RESPONSIBLE FOR ANYTHING THAT HAPPENS TO YOUR WEBSITE
+// CAN USE THIS API FOR PLAYER DATA - https://s6.imrann.my.id/player.json
+
+
 createRoot(document.getElementById('root')).render(
+
+ 
+  
+    
   <StrictMode>
     <div class="bg-white min-h-screen">
     <App />
